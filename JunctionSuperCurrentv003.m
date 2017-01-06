77c79c924fa8da581e8ca59633185374773676c4
ehuemill <ehuemill@gmail.com>
2017-01-06 15:59:00 -0600

    Adapted for Squid and JJ Simulations
    In version 003 I added a squid loop to make it so that there could be a
    jump in the phase of the second half of the junction.  This worked well
    but was clunky.  I then adapted version 004 so that it could cleanly
    handle a squid loop and two junctions of different areas and critical
    currents.  These simulations still assume a small BetaL parameter, adn
    as a result do not include any screening currents contributing to the
    phase in the squid loop.

--- JunctionSuperCurrentv003.m
+++ JunctionSuperCurrentv003.m
@@ -1,8 +1,5 @@
 %Explanation of base program
 
-%This is off the hook!
-
-
 %The program splits a single junciton up into xmax discrete sections.  Each
 %section has a supercurrent density, and a phase difference that is
 %contributed to by the field (PhaseF), and an arbitrary phase set at x=1
@@ -15,42 +12,58 @@
 %lab.  
 
 
+%Version 003 was adapted to add a squid loop into the middle of the
+%junction by including an area that would add to the overall phase of the 
+%part of the junction that is past the location of the loop.  The loop size
+%can be varied, as can the relative critical currents between the two areas
+%
+
+
 
 %% Clearing memory and input screen.
 
 clear;
 clc;
 close all;
-%% Defining the Parameters of the Simulaiton
-xmax=101;
-x(1,:)=(1:xmax);
 
-%Flux Loop Parameters
+%% Defining the Parameters of the Simulaiton
+xmax1=201;
+x1(1,:)=(1:xmax);
+
+xmax2=201;
+x2(1,:)=(1:xmax);
+
+LoopArea=1;
+
+
+%Field Parameters
 f=1;
-fmax=201;
-FluxinJuncMin=-3;
-FluxinJuncMax=3;
+fmax=1001;
+FieldMin=-5;
+FieldMax=5;
 
 
 %Phase Loop parameters
 p=1;
-pmax=101;
+pmax=201;
 Phase1Min=0*pi;
-Phase1Max=2*pi;
+Phase1Max=4*pi;
 
 
 %Pre Allocating memory to the arrays to decrease runtime
 Phase1=zeros(1,pmax);
-FluxinJunc=zeros(1,fmax);
+FluxinJunc1=zeros(1,fmax);
+FluxinJunc2=zeros(1,fmax);
+FluxinLoop=zeros(1,fmax);
 
 SCurrentDensityNoise=(2*rand(1,xmax)-1);
-SCurrentDensity=ones(1,xmax)+0.5*SCurrentDensityNoise;
+SCurrentDensity=ones(1,xmax)+.01*SCurrentDensityNoise;
 
 
 SCurrent=zeros(xmax,pmax,fmax);
 SCurrentNet=zeros(1,pmax);
 MaxSCurrentNet=zeros(1,fmax);
-
+MinSCurrentNet=zeros(1,fmax);
 %% Loops for running the simulation Meat of the Simulation
 
 
@@ -62,26 +75,32 @@ for f=1:fmax
 
     FluxinJunc(f)=FluxinJuncMin+(f-1)*FluxinJuncSS;
     PhaseF=2*pi*x./xmax*FluxinJunc(f);
-    %Phase1 Loop of externally set phase in 
+    
+    FluxinLoop(f)=FluxinJuncMin+(f-1)*FluxinJuncSS;
+    PhaseL=2*pi*LoopArea*FluxinLoop(f);
+    
+    %Phase1 Loop of externally set phase 
     %Define the loop setp size, then run the for loop
-    Phase1SSS=(Phase1Max-Phase1Min)/(pmax-1);
+    Phase1SS=(Phase1Max-Phase1Min)/(pmax-1);
     for p=1:pmax
 
-        Phase1(p)=Phase1Min+(p-1)*Phase1SSS;
-
-        SCurrent=SCurrentDensity.*sin(PhaseF+Phase1(p));
+        Phase1(p)=Phase1Min+(p-1)*Phase1SS;
+        PhaseDrop=PhaseF+Phase1(p)+PhaseL;
+        SCurrent=SCurrentDensity.*(sin(PhaseDrop));
         SCurrentNet(p)=sum(SCurrent)/xmax;
 
 
     end
 
     MaxSCurrentNet(f)=max(SCurrentNet);
-
+    MinSCurrentNet(f)=min(SCurrentNet);
 end
 
 
 figure
-plot(FluxinJunc,MaxSCurrentNet)
+plot(FluxinJunc,MaxSCurrentNet,'r.')
+hold on 
+plot(FluxinJunc,MinSCurrentNet,'g.')
 
 
 
