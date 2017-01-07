2f13323ab32c948721a54392deb9aa54ffab5f3a
ehuemill <ehuemill@gmail.com>
2017-01-06 20:23:01 -0600

    Comparison in Plot with Jam variable
    I added an extra loop on the outside of the meat of the program so that
    it would run once with Jam=0, and then once with jam=1.  That made it so
    that when it plots its easy to compare the conventional plot to any
    plots with added CPR or noise features.

--- JunctionSuperCurrentv004.m
+++ JunctionSuperCurrentv004.m
@@ -1,26 +1,27 @@
 %Explanation of base program
 
-%The program splits a single junciton up into xmax discrete sections.  Each
+%The program splits a squid into two junctions that have xmax discrete sections.  Each
 %section has a supercurrent density, and a phase difference that is
-%contributed to by the field (PhaseF), and an arbitrary phase set at x=1
-%called Phase1.  For each value of field and Phase1 the supercurrent at
-%each point is calculated and the net supercurrent (sum along the junction)
+%contributed to by the field (PhaseF) in the junction, the field in the squid loop,
+%and an arbitrary phase set at x=1 %called Phase1.  These phase factors are 
+%all summed along the junction to determine the local phase difference 
+%across the junction at each point.  For each value of field and Phase1 the
+%supercurrent at each point is calculated and the net supercurrent (sum 
+%along the junction and then the sum of the two junctions together)
 %is calcualted to find the supercurrent carried at that phase and that
 %field.  To find the maximum supercurrent for a given field, the max of
-%that vector is taken, and can be plotted against the magnetic field.  This
-%is the exact measurment of the critical current vs field that we do in the
-%lab.  
+%that vector is taken for that field value.  The minimum can be calculated 
+%to find the negative critical current.  Both of these can be plotted 
+%against the magnetic field that is applied.  This is the exact measurment 
+%of the critical current vs field that we do in the lab.
 
+%This version adds the ability to add several realistic features of squids
 
-%Version 003 was adapted to add a squid loop into the middle of the
-%junction by including an area that would add to the overall phase of the 
-%part of the junction that is past the location of the loop.  The loop size
-%can be varied, as can the relative critical currents between the two areas
-%
-% Version 004 Was adapted so that there were two totally separate junctions
-% that both can modulate, but that the phases are pinned together at one
-% end through the squid loop area. This simulation includes junction area
-% asymmetry, junction critical current asymmetry, 
+% Critical Current Asymmetry
+% Junction Area Asymmetry
+% Critical Current density variation along each junction
+% Squid Loop size variation
+% Changes in the field scan range
 
 
 
@@ -34,50 +35,57 @@
 
 
 %% Clearing memory and input screen
-% 
-% clear;
-% clc;
-% close all;
+
+clear;
+clc;
+close all;
 
 %% Defining the Parameters of the Simulaiton
 
 %Setting Junction 1 Parameters
-xmax1=101;
-x1(1,:)=(1:xmax1);
-JuncSCMag1=1;
-JuncWid1=.5;
-JuncLen1=1;
+    xmax1=201;
+    xmax2=201;
+    x1(1,:)=(1:xmax1);
+    x2(1,:)=(1:xmax2);
+
+%Critical Current Magnitudes
+    JuncSCMag1=2.5;
+    JuncSCMag2=5;
+
+%Junction Area Determinination
+    JuncWid1=.5;
+    JuncLen1=1;
+
+    JuncWid2=2;
+    JuncLen2=1;
+
 JuncArea1=JuncWid1*JuncLen1;
-
-SCurNoise1=(2*rand(1,xmax1)-1);
-SCurDen1=JuncSCMag1*ones(1,xmax1)/xmax1+.01*SCurNoise1/xmax1;
-
-%Setting Junction 2 Parameters
-xmax2=101;
-x2(1,:)=(1:xmax2);
-JuncSCMag2=50;
-JuncWid2=.5;
-JuncLen2=1;
 JuncArea2=JuncWid2*JuncLen2;
 
-SCurNoise2=(2*rand(1,xmax2)-1);
-SCurDen2=JuncSCMag2*ones(1,xmax2)/xmax2+.01*SCurNoise2/xmax2;
+%Current Noise Parameters
+    SCurNoise1=(2*rand(1,xmax1)-1);
+    SCurNoise2=(2*rand(1,xmax2)-1);
+
+%Calculating Critical Current Densities
+    SCurDen1=JuncSCMag1*ones(1,xmax1)/xmax1+.01*SCurNoise1/xmax1;
+    SCurDen2=JuncSCMag2*ones(1,xmax2)/xmax2+.01*SCurNoise2/xmax2;
+
 
 %Setting Squid Loop Parameers
-LoopWid=10;
-LoopLen=10;
+LoopWid=2;
+LoopLen=4;
 LoopArea=LoopWid*LoopLen;
 
 %Field Parameters
 f=1;
-fmax=1001;
-FieldMin=-.03;
-FieldMax=.03;
+fmax=2001;
+FieldMin=-1;
+FieldMax=1;
 
 
 %Phase Loop parameters
 p=1;
-pmax=401;
+pmax=201;
 Phase0Min=0*pi;
 Phase0Max=4*pi;
 
@@ -97,53 +105,60 @@ SCurrent1=zeros(xmax1,pmax,fmax);
 SCurrent2=zeros(xmax2,pmax,fmax);
 SCurrentNet=zeros(1,pmax);
 
-MaxSCurrentNet=zeros(1,fmax);
-MinSCurrentNet=zeros(1,fmax);
+MaxSCurrentNet=zeros(2,fmax);
+MinSCurrentNet=zeros(2,fmax);
 %% Loops for running the simulation Meat of the Simulation
 
 
-
-%Field Contribution to the Phase 
-%Define the Field ForLoop setp size, then run the Field for ForLoop
-FieldSS=(FieldMax-FieldMin)/(fmax-1);
-for f=1:fmax
-
-    Field(f)=FieldMin+(f-1)*FieldSS;
-    
-    
-    PhaseF1=2*pi*Field(f)*JuncArea1;
-    PhaseF2=2*pi*Field(f)*JuncArea2;
-    PhaseFL=2*pi*Field(f)*LoopArea;
-    
-    PhaseFDen1=PhaseF1*x1/xmax1;
-    PhaseFDen2=PhaseF2*x2/xmax2;
-    
-    %Phase0 ForLoop of externally set phase 
-    %Define the Phase0 setp size, then run the ForLoop
-    Phase0SS=(Phase0Max-Phase0Min)/(pmax-1);
-    for p=1:pmax
-
-        Phase0(p)=Phase0Min+(p-1)*Phase0SS;
-        
-        PhaseDrop1=Phase0(p)+PhaseFDen1;
-        PhaseDrop2=Phase0(p)+PhaseF1++PhaseFL+PhaseFDen2;
-                            
-        SCurrent1=SCurDen1.*(sin(PhaseDrop1)+sin(2*PhaseDrop1));
-        SCurrent2=SCurDen2.*(sin(PhaseDrop2));
-        
-        SCurrentNet(p)=sum(SCurrent1)+sum(SCurrent2);
+for j=1:2;
+    Jam=1-j;
 
 
+    %Field Contribution to the Phase 
+    %Define the Field ForLoop setp size, then run the Field for ForLoop
+    FieldSS=(FieldMax-FieldMin)/(fmax-1);
+    for f=1:fmax
+
+        Field(f)=FieldMin+(f-1)*FieldSS;
+
+
+        PhaseF1=2*pi*Field(f)*JuncArea1;
+        PhaseF2=2*pi*Field(f)*JuncArea2;
+        PhaseFL=2*pi*Field(f)*LoopArea;
+
+        PhaseFDen1=PhaseF1*x1/xmax1;
+        PhaseFDen2=PhaseF2*x2/xmax2;
+
+        %Phase0 ForLoop of externally set phase 
+        %Define the Phase0 setp size, then run the ForLoop
+        Phase0SS=(Phase0Max-Phase0Min)/(pmax-1);
+        for p=1:pmax
+
+            Phase0(p)=Phase0Min+(p-1)*Phase0SS;
+
+            PhaseDrop1=Phase0(p)+PhaseFDen1;
+            PhaseDrop2=Phase0(p)+PhaseF1+PhaseFL+PhaseFDen2;
+
+            SCurrent1=SCurDen1.*((1-.2*Jam)*sin(PhaseDrop1)+(.2)*Jam*sin(PhaseDrop1*2));
+            SCurrent2=SCurDen2.*(sin(PhaseDrop2));
+
+            SCurrentNet(p)=sum(SCurrent1)+sum(SCurrent2);
+
+
+        end
+        MaxSCurrentNet(j,f)=max(SCurrentNet);
+        MinSCurrentNet(j,f)=min(SCurrentNet);
     end
-    MaxSCurrentNet(f)=max(SCurrentNet);
-    MinSCurrentNet(f)=min(SCurrentNet);
+
 end
 
-
 hold on
-plot(Field,MaxSCurrentNet,'r.')
+plot(Field,MaxSCurrentNet,'.')
+xlabel('Magnetic Field'); ylabel('Critical Current');
+
+
 hold on 
-plot(Field,MinSCurrentNet,'g.')
+plot(Field,MinSCurrentNet,'.')
 
 
 
