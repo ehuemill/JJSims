b48b080cadd707744e8c8caccb9c5b66122e7ea7
ehuemill <ehuemill@gmail.com>
2017-01-07 21:43:12 -0600

    Added the Phase Plot and cleaned the code up
    I added the phase variable and plot to the proram and cleaned up some of
    the spacing in the description.  I also added labels to the graph at the
    end.

--- JunctionSuperCurrentv001.m
+++ JunctionSuperCurrentv001.m
@@ -1,8 +1,5 @@
 %Explanation of base program
 
-%This is off the hook!
-
-
 %The program splits a single junciton up into xmax discrete sections.  Each
 %section has a supercurrent density, and a phase difference that is
 %contributed to by the field (PhaseF), and an arbitrary phase set at x=1
@@ -14,40 +11,41 @@
 %is the exact measurment of the critical current vs field that we do in the
 %lab.  
 
-
-
 %% Clearing memory and input screen.
-
 clear;
 clc;
 close all;
 %% Defining the Parameters of the Simulaiton
+
+%Defining number of discrete steps in the junction
 xmax=101;
 x(1,:)=(1:xmax);
 
 %Flux Loop Parameters
 f=1;
-fmax=201;
+fmax=601;
 FluxinJuncMin=-3;
 FluxinJuncMax=3;
+
+SCurrentMag =1;
+SCurrentNoiseMag =.5;
 
 
 %Phase Loop parameters
 p=1;
 pmax=101;
-Phase1Min=0*pi;
-Phase1Max=2*pi;
+Phase1Min=-0*pi;
+Phase1Max=2.0*pi;
 
 
 %Pre Allocating memory to the arrays to decrease runtime
 Phase1=zeros(1,pmax);
 FluxinJunc=zeros(1,fmax);
-
-SCurrentDensityNoise=(2*rand(1,xmax)-1);
-
-SCurrentDensity=ones(1,xmax)+1*SCurrentDensityNoise;
+IndexMax=zeros(1,fmax);
+Phase1MaxSC=zeros(1,fmax);
 
 
+SCurrentDensity=(SCurrentMag*ones(1,xmax)+SCurrentNoiseMag*(2*rand(1,xmax)-1))/xmax;
 
 SCurrent=zeros(xmax,pmax,fmax);
 SCurrentNet=zeros(1,pmax);
@@ -56,34 +54,39 @@ MaxSCurrentNet=zeros(1,fmax);
 %% Loops for running the simulation Meat of the Simulation
 
 
-
 %Field Contribution to the Phase 
-%Define the loop setp size, then run the for loop
+%Define the loop step size, then run the for loop
 FluxinJuncSS=(FluxinJuncMax-FluxinJuncMin)/(fmax-1);
 for f=1:fmax
 
     FluxinJunc(f)=FluxinJuncMin+(f-1)*FluxinJuncSS;
-    PhaseF=2*pi*x./xmax*FluxinJunc(f);
-    %Phase1 Loop of externally set phase in 
-    %Define the loop setp size, then run the for loop
+    PhaseF=2*pi*FluxinJunc(f)*x./xmax;
+    
+    
+%Phase1 Loop of externally set phase at edge of JJ 
+    %Define the loop step size, then run the for loop
     Phase1SS=(Phase1Max-Phase1Min)/(pmax-1);
+    
     for p=1:pmax
-
         Phase1(p)=Phase1Min+(p-1)*Phase1SS;
         
-        SCurrent=SCurrentDensity.*sin(PhaseF+Phase1(p));
-        SCurrentNet(p)=sum(SCurrent)/xmax;
-      
-
+        PhaseDrop=Phase1(p)+PhaseF;
+        
+        SCurrent=SCurrentDensity.*sin(PhaseDrop);
+        SCurrentNet(p)=sum(SCurrent);
     end
-
-    MaxSCurrentNet(f)=max(SCurrentNet);
-
+     
+    [MaxSCurrentNet(f),IndexMax(f)]=max(SCurrentNet);
+    Phase1MaxSC(f)=Phase1(IndexMax(f));
+        
 end
 
-
 figure
-plot(FluxinJunc,MaxSCurrentNet)
+subplot(2,1,1); plot(FluxinJunc,MaxSCurrentNet,'.')
+ylabel('Critical Current');
+
+subplot(2,1,2); plot(FluxinJunc,Phase1MaxSC/pi,'.')
+xlabel('Flux');ylabel('Phase1 of Ic/pi');
 
 
 
